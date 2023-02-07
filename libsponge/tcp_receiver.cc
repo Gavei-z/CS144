0eaa64d9430741d14850a9c7102f125c3a7d560c
Gavei-z <1026352414@qq.com>
2023-02-08 01:21:08 +0800

    add some conments for lab2

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,12 +11,21 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
+    /**
+     * The first SYN is useful, the initial sequence number
+     * should be set after that.
+     */
     if (seg.header().syn && !_isn.has_value()) {
 //        _syn = true;
         _isn.emplace(seg.header().seqno);
 //        _ack.emplace(_isn.value());
     }
 
+    /**
+     * The function `bytes_written()` of the class `ByteStream` is always
+     * pointing to the absolute current start window position. It is useful
+     * when we push_substring(as the checkpoint).
+     */
     if (_isn.has_value()) {
         _reassembler.push_substring(
                 seg.payload().copy(),
@@ -24,15 +33,23 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
                 seg.header().fin
                 );
         _ack.emplace(wrap(stream_out().bytes_written(), _isn.value()));
-
     }
 
+    /**
+     * The payload is empty when the first SYN comes, and `_ack` == `_isn`,
+     * we should deal with this situation.
+     */
     if (!_syn && seg.header().syn && _ack.has_value()) {
         _syn = true;
         _ack.emplace(_ack.value() + 1);
         _isn.emplace(_ack.value());
     }
 
+    /**
+     * The connection was set up, the _fin == true, and all the bytes
+     * were written, `_ack` plus 1. Cause the receiver need the second FIN
+     * from the sender.
+     */
     _fin = _fin ? _fin : seg.header().fin;
     if (_isn.has_value() && _fin && _reassembler.stream_out().input_ended()) {
         _ack = WrappingInt32(_ack.value() + 1);
